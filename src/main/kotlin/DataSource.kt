ad3e41a9f8bcbb886c8e76ae14df969fdbc00b00
slava_ivanov <tristandtm@gmail.com>
2025-10-04 09:18:13 +0300

    add hikari, add working with dirs

---
+++ src/main/kotlin/DataSource.kt
@@ -0,0 +1,15 @@
+package ru.cib
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+
+class DataSource {
+
+    fun getHikariConnection() = HikariDataSource(HikariConfig().apply {
+            driverClassName = "org.postgresql.Driver"
+            jdbcUrl = "jdbc:postgresql://localhost:5432/postgres"
+            username = "postgres"
+            password = "test"
+            maximumPoolSize = 30
+        }).connection
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,19 +5,35 @@ import java.sql.DriverManager
 import javax.xml.bind.JAXBContext
 
 fun main() {
-
     //Read XML with Jaxb
-    val readFile = File("/Users/slava_ivanov_saikyo/2025-q1-kotlin/src/main/resources/students.xml")
-    val context = JAXBContext.newInstance(Students::class.java)
-    val unmarshaller = context.createUnmarshaller()
-    val students = unmarshaller.unmarshal(readFile) as Students
+    val newDir = File("/Users/slava_ivanov_saikyo/2025-q1-kotlin/src/main/resources/new")
+    newDir.walk().forEach { file ->
+        if (file.isFile) {
+            try {
+                val context = JAXBContext.newInstance(Students::class.java)
+                val unmarshaller = context.createUnmarshaller()
+                val students = unmarshaller.unmarshal(file) as Students
 
-    //Database connection and execute
-    val connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "test")
-    students.student.forEach { student ->
-        connection.prepareStatement("Insert into test (name, surname, birthday) values ('${student.name}', '${student.surname}', '${student.birthday}')").use {
-            it.execute()
+                //Database connection and execute
+                val connection = DataSource().getHikariConnection()
+                students.student.forEach { student ->
+                    connection.prepareStatement("Insert into test (name, surname, birthday) values ('${student.name}', '${student.surname}', '${student.birthday}')")
+                        .use {
+                            it.execute()
+                        }
+                }
+                connection.close()
+
+                file
+                    .copyTo(File("/Users/slava_ivanov_saikyo/2025-q1-kotlin/src/main/resources/archive/${file.name}"))
+                file.delete()
+            } catch (e: Exception) {
+                println(e.message)
+                file.copyTo(
+                    File("/Users/slava_ivanov_saikyo/2025-q1-kotlin/src/main/resources/error/${file.name}")
+                )
+                file.delete()
+            }
         }
     }
-    connection.close()
 }
